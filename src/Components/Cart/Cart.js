e13993276c8f608db69aaf1f0d6ccb696fb32652
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-12-02 15:59:16 +0600

    calculation

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,14 +3,19 @@ import React from 'react';
 const Cart = ({cart}) => {
    
 
-    let total =0;
+    console.log(cart);
+   
+
+    let total = 0;
     for(const product of cart){
-        total = total + product.time;
+        total = parseFloat(total) + parseFloat(product.time);
     }
+  
     return (
         <div>
+            <p>selected item : {cart.length}</p>
             <h2>Study Details</h2>
-            <h1>Study Time:{total}m </h1>
+            <h1>Study Time:{total} </h1>
             <h1>Break Time: </h1>
 
         </div>
